dbc8aad0377b7d767cf6ad9bde791185eeb75ada
NAMANJAINCHICMIC <naman.jain@chicmic.co.in>
2023-02-23 18:39:19 +0530

    hello

--- src/app/data-display/data-display.component.ts
+++ src/app/data-display/data-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-data-display',
@@ -16,18 +16,26 @@ export class DataDisplayComponent implements OnInit {
 
 
   @Input() url:string = "";
-  public selectedStatus : any;
+  // public selectedStatus : any;
   onSelected(value:string , sts:string):void{
     // this.selectedStatus= value;
     sts = value;
     // this.data=value;
+    // this.http.put(this.url , this.data).subscribe(datas => {
+    //   console.log(datas);
+    // });
+  }
+  @Output()
+  statusChange : EventEmitter<{index:string , value: string}> =new EventEmitter<{index:string , value: string}>() 
+  onChange(index:string , value: string){
+    this.statusChange.emit({index:index , value: value})
   }
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get(this.url).subscribe((res)=>{
-      this.selectedStatus = res;
-    })
+    // this.http.get(this.url).subscribe((res)=>{
+    //   this.selectedStatus = res;
+    // })
 
   }
 }
--- src/app/milestone/milestone.component.ts
+++ src/app/milestone/milestone.component.ts
@@ -8,12 +8,25 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MilestoneComponent implements OnInit{
   public userInfo : any;
+  public url: string = "http://localhost:3000/data2";
+  onStatusChange(data:any){
+    this.http.put(this.url , this.userInfo).subscribe(datas => {
+        console.log(datas);
+      });
+      this.http.get(this.url).subscribe((res)=>{
+        this.userInfo=res;
+      });
+  }
+
   public constructor(private http:HttpClient){}
   public ngOnInit(): void {
       const url: string = 'http://localhost:3000/data2';
       this.http.get(url).subscribe((res)=>{
         this.userInfo=res;
-      })
-  }
+      });
+      // this.http.put(this.url , this.userInfo).subscribe(datas => {
+      //   console.log(datas);
+      // });
 
+}
 }
