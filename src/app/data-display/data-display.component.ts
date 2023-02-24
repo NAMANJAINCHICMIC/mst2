d59c3a844688c988a650b2967d82e735c14ca45f
NAMANJAINCHICMIC <naman.jain@chicmic.co.in>
2023-02-24 18:07:02 +0530

    task incomp

--- src/app/data-display/data-display.component.ts
+++ src/app/data-display/data-display.component.ts
@@ -1,30 +1,62 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-data-display',
   templateUrl: './data-display.component.html',
   styleUrls: ['./data-display.component.css']
 })
-export class DataDisplayComponent {
-  [x: string]: any;
+export class DataDisplayComponent implements OnChanges{
+  // [x: string]: any;
+  
 
   @Input()
   data: {
     id:number,
-    name: string;
-    status: string;
-  }[] = [{id: +"",name: "", status: ""}];
+    name: string,
+    status: string, 
+    message: string
+  }[] = [{id: +"",name: "", status: "", message: ""}];
 
 
-  @Input() url = "";
+  // @Input() url = "";
 
   @Output()
   statusChange : EventEmitter<{item:any }> =new EventEmitter<{item:any }>() 
+  @Output()
+  statusUnChange : EventEmitter<void> =new EventEmitter<void>() 
   onChange(item:any){
+    if(item.status == "hold" ){
+    const msg = prompt("please enter your reason");
+    if (msg == null || msg ==""){
+
+      // window.location.reload();
+      
+      this.statusUnChange.emit();
+      return ;
+    }
+    else{
+      item.message = msg;
+      this.statusChange.emit(item );
+      return;
+    }
+    }
+    
     this.statusChange.emit(item);
 
   }
   constructor(private http:HttpClient) { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    // if(this.statusUnChange){
+    //   console.log("hello")
+    //   changes['data'].currentValue = changes['data'].previousValue;
+    // }
+   
+    
+    // throw new Error('Method not implemented.');
+  }
+  
 
+ 
 
 }
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit , OnChanges, DoCheck, SimpleChanges, AfterContentChecked} from '@angular/core';
 
 @Component({
   selector: 'app-project',
@@ -15,16 +15,30 @@ export class ProjectComponent implements OnInit{
     this.http.put(this.url+"/" + data.id, data).subscribe(datas => {
         console.log(datas);
       });
+   
+    
+  }
+  onStatusUnChange(){
+    
+    this.http.get(this.url).subscribe((res)=>{
+      this.userInfo=res;
+      console.log(res);
+    });
+
     
   }
 
-  public constructor(private http:HttpClient){}
-  public ngOnInit(): void {
+  public constructor(private http:HttpClient){this.http.get(this.url).subscribe((res)=>{
+      this.userInfo=res;
+    });}
+ ngOnInit(): void {
      
       this.http.get(this.url).subscribe((res)=>{
         this.userInfo=res;
       });
-   
+ 
 
     }
+
+
 }
